a6dca1a748501acdd0b8d749a1923ec7959d5a63
Jane1217 <yanxinch@sas.upenn.edu>
2025-12-09 16:47:47 -0500

    Implement all user routes in userRoutes.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const userRoutes = require("./routes/userRoutes");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -9,5 +11,14 @@ app.use(express.json());
 app.get("/api", (req, res) => {
   res.json({ message: "Backend is running" });
 });
+
+// User Routes
+app.post("/users/", userRoutes.createUser);
+app.get("/users/:user_id", userRoutes.userAccounts);
+app.patch("/users/:user_id", userRoutes.updateUser);
+app.post("/music/playlists/:playlist_id/save", userRoutes.savePlaylist);
+app.delete("/music/playlists/:playlist_id/save", userRoutes.deleteSavedPlaylist);
+app.get("/users/:user_id/games", userRoutes.getUserGames);
+app.get("/users/:user_id/playlists", userRoutes.getUserPlaylists);
 
 app.listen(5000, () => console.log("Server running on port 5000"));
